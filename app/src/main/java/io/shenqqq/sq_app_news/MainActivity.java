6fd73e67ac21f7cb5d7eab0b7b2afe37fb043a65
ShenQi <2018211815@stu.cqupt.edu.cn>
2021-06-04 16:34:43 +0800

    登录界面完成

---
+++ app/src/main/java/io/shenqqq/sq_app_news/MainActivity.java
@@ -0,0 +1,14 @@
+package io.shenqqq.sq_app_news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    //启动界面
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
